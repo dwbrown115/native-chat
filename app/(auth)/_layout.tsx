92a3d22e3a6cf168989381af82d0d5315d75234f
dwbrown115 <114196159+dwbrown115@users.noreply.github.com>
2024-04-27 06:41:00 -0500

    created auth folder and files to go with it

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -7,15 +7,14 @@ import Colors from "@/constants/Colors";
 import { useColorScheme } from "@/components/useColorScheme";
 import { useClientOnlyValue } from "@/components/useClientOnlyValue";
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
+function AuthBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
-export default function TabLayout() {
+export default function AuthLayout() {
   const colorScheme = useColorScheme();
 
   return (
@@ -28,32 +27,32 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="login"
         options={{
-          title: "Tab One",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: "Login",
+          tabBarIcon: ({ color }) => <AuthBarIcon name="code" color={color} />,
           headerRight: () => (
-            <Link href="/login" asChild>
+            <Link href="/signup" asChild>
               <Pressable>
                 {/* {({ pressed }) => (
                   <FontAwesome
                     name="info-circle"
                     size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
+                    color={Colors[colorScheme ?? "light"].text}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                   />
                 )} */}
-                <Text style={{ color: "blue", marginRight: 10 }}>Login</Text>
+                <Text style={{ color: "blue", marginRight: 10 }}>Signup</Text>
               </Pressable>
             </Link>
           ),
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="signup"
         options={{
-          title: "Tab Two",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: "Signup",
+          tabBarIcon: ({ color }) => <AuthBarIcon name="code" color={color} />,
         }}
       />
     </Tabs>
